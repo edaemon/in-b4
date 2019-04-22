4ff13d58eaeb00062b2705920cc2c14e3bf0e007
Evan Damon <evan.damon@tektronix.com>
2019-04-22 16:53:38 -0700

    For #1, add date time picker to the form

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,27 +1,37 @@
 import React, { Component } from 'react';
+import DateTimePicker from 'react-datetime-picker';
 
 class MessageForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: 'Enter message here'};
+        this.state = {
+            value: 'Enter message here',
+            datetime: new Date(),
+        }
 
-        this.handleChange = this.handleChange.bind(this);
+        this.handleDateTimeChange = this.handleDateTimeChange.bind(this);
+        this.handleMessageChange = this.handleMessageChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(event) {
+    handleMessageChange(event) {
         this.setState({value: event.target.value});
     }
 
+    handleDateTimeChange(datetime) {
+        this.setState({datetime});
+    }
+
     handleSubmit(event) {
-        alert('Message value: ' + this.state.value);
+        alert('Message value: ' + this.state.value + '; date-time: ' + this.state.datetime);
         event.preventDefault();
     }
 
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
-                <textarea value={this.state.value} onChange={this.handleChange} />
+                <textarea value={this.state.value} onChange={this.handleMessageChange} />
+                <DateTimePicker value={this.state.datetime} onChange={this.handleDateTimeChange} />
                 <input type="submit" value="Submit" />
             </form>
         );
